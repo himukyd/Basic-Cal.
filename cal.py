10e1236dff137a10799f6cc9641ee97de29d6755
Himanshu <himukyd1@gmail.com>
2025-05-24 08:34:07 +0530

    Adding html and python file.

---
+++ cal.py
@@ -0,0 +1,16 @@
+# Basic Calu for Addition + - * and divission 
+
+num_1 = float("Enter ist number :")
+num_2 = float("Enter 2nd number :")
+
+def add(num_1 , num_2):
+    return(num_1+num_2)
+
+def sub(num_1 , num_2):
+    return(num_1-num_2)
+
+def mul(num_1 , num_2):
+    return(num_1*num_2)
+
+def div(num_1 , num_2):
+    return(num_1/num_2)
